cf0455b281a1255ac4abff7de57649dc54a22b77
kimyeontae <kimyeont960@gmail.com>
2024-09-08 20:01:01 +0900

    0908 작업

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const range = require("lodash/range");
+
+const pxToRem = (px, base = 16) => `${px / base}rem`;
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,6 +13,30 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      borderRadius: {
+        ...range(1, 100).reduce((acc, px) => {
+          acc[`${px}pxr`] = pxToRem(px);
+          return acc;
+        }, {}),
+      },
+      fontSize: {
+        ...range(1, 100).reduce((acc, px) => {
+          acc[`${px}pxr`] = pxToRem(px);
+          return acc;
+        }, {}),
+      },
+      spacing: {
+        ...range(1, 100).reduce((acc, px) => {
+          acc[`${px}pxr`] = pxToRem(px);
+          return acc;
+        }, {}),
+      },
+      gap: {
+        ...range(1, 100).reduce((acc, px) => {
+          acc[`${px}pxr`] = pxToRem(px);
+          return acc;
+        }, {}),
       },
     },
   },
