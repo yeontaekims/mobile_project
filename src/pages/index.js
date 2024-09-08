749cec13be3099155912d454a85d57d32f9ed765
kimyeontae <kimyeont960@gmail.com>
2024-09-08 19:12:49 +0900

    디벨롭 브런치 변경

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            이곳은 디벨롭 브런치입니다.{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/pages/index.js!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             </code>
